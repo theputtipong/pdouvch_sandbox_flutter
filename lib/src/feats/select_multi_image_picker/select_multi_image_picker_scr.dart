7df3e65fb7c4cd6348bf9e541dc2147065bf32c2
PuttipongD <puttipong.dou@gmail.com>
2024-08-09 21:06:34 +0700

    update

--- lib/src/feats/select_multi_image_picker/select_multi_image_picker_scr.dart
+++ lib/src/feats/select_multi_image_picker/select_multi_image_picker_scr.dart
@@ -18,105 +18,147 @@ class SelectMultiImagePicker extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Select Multi Image Picker'),
       ),
-      body: Obx(
-        () => ctl.isLoading.value
-            ? buildCircularProgressIndicator()
-            : ListView.builder(
-                shrinkWrap: true,
-                physics: const BouncingScrollPhysics(),
-                itemCount: ctl.photoPlots.length,
-                itemBuilder: (BuildContext context, int index) {
-                  ImagePlot valuePlot = ctl.photoPlots[index];
-                  return Column(
+      body: Obx(() => ctl.isLoading.value
+          ? buildCircularProgressIndicator()
+          : ListView(
+              shrinkWrap: true,
+              children: [
+                SizedBox(
+                  height: 100,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      InkWell(
+                        onTap: () async {
+                          // valuePlot.fileUpload = await ctl.pickFile(ImageSource.camera);
+                          // ctl.trickImage.refresh();
+                        },
+                        child: const Column(
+                          children: [
+                            Icon(
+                              Icons.camera_alt_outlined,
+                              size: 40,
+                            ),
+                            Text('ถ่ายภาพ'),
+                          ],
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () async {
+                          // valuePlot.fileUpload = await ctl.pickFile(ImageSource.gallery);
+                          // ctl.trickImage.refresh();
+                        },
+                        child: const Column(
+                          children: [
+                            Icon(
+                              Icons.photo_size_select_actual_rounded,
+                              size: 40,
+                            ),
+                            Column(
+                              children: [
+                                Text('เลือกภาพจากคลังภาพ'),
+                                Text(
+                                  '(ครั้งละไม่เกิน 10 รูป)',
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(),
+                Obx(() => ctl.photoPlots.isEmpty ? const Center(child: Text('ไม่พบรูปภาพ')) : buildGrid(ctl)),
+              ],
+            )),
+    );
+  }
+
+  GridView buildGrid(SelectMultiImagePickerCtl ctl) {
+    return GridView.builder(
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, crossAxisSpacing: 5.0, mainAxisSpacing: 5.0, childAspectRatio: 0.75),
+      itemCount: ctl.photoPlots.length,
+      itemBuilder: (context, index) {
+        ImagePlot valuePlot = ctl.photoPlots[index];
+        return Obx(
+          () => ctl.trickImage.value
+              ? const Center(child: CircularProgressIndicator())
+              : Column(
+                  children: [
+                    valuePlot.fileUpload != null && valuePlot.fileUpload!.path.isNotEmpty
+                        ? InkWell(
+                            onTap: () async {
+                              // String image = valuePlot.fileUpload!.path;
+                              // await Get.to(HeroImagePage(image: image, isLocal: true));
+                            },
+                            child: Image.file(
+                              File(valuePlot.fileUpload!.path),
+                              height: 120,
+                              width: 120,
+                              fit: context.isLandscape ? BoxFit.fitHeight : BoxFit.fitWidth,
+                            ),
+                          )
+                        : InkWell(
+                            onTap: () async {
+                              // String image = '${ctl.baseUrl}${valuePlot.after}';
+                              // bool checkImage = await ctl.checkImageUrl(image);
+                              // if (checkImage) {
+                              //   await Get.to(HeroImagePage(image: image, isLocal: false));
+                              // }
+                            },
+                            child: CachedNetworkImage(
+                              imageUrl: 'http://68.183.187.67:8000${valuePlot.image}',
+                              placeholder: (context, url) => SizedBox(
+                                height: 120,
+                                width: 120,
+                                child: LinearProgressIndicator(
+                                  color: Get.theme.primaryColor,
+                                ),
+                              ),
+                              errorWidget: (context, url, error) => const SizedBox(
+                                height: 120,
+                                width: 120,
+                                child: Icon(Icons.error),
+                              ),
+                              fit: context.isLandscape ? BoxFit.fitHeight : BoxFit.fitWidth,
+                              height: 120,
+                              width: 120,
+                            ),
+                          ),
+                    SizedBox(
+                      height: 40,
+                      width: 120,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          Column(
-                            children: [
-                              Obx(
-                                () => ctl.trickImage.value
-                                    ? const Center(child: CircularProgressIndicator())
-                                    : Column(
-                                        children: [
-                                          valuePlot.fileUpload != null && valuePlot.fileUpload!.path.isNotEmpty
-                                              ? InkWell(
-                                                  onTap: () async {
-                                                    // String image = valuePlot.fileUpload!.path;
-                                                    // await Get.to(HeroImagePage(image: image, isLocal: true));
-                                                  },
-                                                  child: Image.file(
-                                                    File(valuePlot.fileUpload!.path),
-                                                    height: 120,
-                                                    width: 120,
-                                                    fit: context.isLandscape ? BoxFit.fitHeight : BoxFit.fitWidth,
-                                                  ),
-                                                )
-                                              : InkWell(
-                                                  onTap: () async {
-                                                    // String image = '${ctl.baseUrl}${valuePlot.after}';
-                                                    // bool checkImage = await ctl.checkImageUrl(image);
-                                                    // if (checkImage) {
-                                                    //   await Get.to(HeroImagePage(image: image, isLocal: false));
-                                                    // }
-                                                  },
-                                                  child: CachedNetworkImage(
-                                                    imageUrl: 'http://68.183.187.67:8000${valuePlot.image}',
-                                                    placeholder: (context, url) => SizedBox(
-                                                      height: 120,
-                                                      width: 120,
-                                                      child: LinearProgressIndicator(
-                                                        color: Get.theme.primaryColor,
-                                                      ),
-                                                    ),
-                                                    errorWidget: (context, url, error) => const SizedBox(
-                                                      height: 120,
-                                                      width: 120,
-                                                      child: Icon(Icons.error),
-                                                    ),
-                                                    fit: context.isLandscape ? BoxFit.fitHeight : BoxFit.fitWidth,
-                                                    height: 120,
-                                                    width: 120,
-                                                  ),
-                                                ),
-                                          SizedBox(
-                                            height: 40,
-                                            child: Row(
-                                              children: [
-                                                IconButton(
-                                                  onPressed: () async {
-                                                    valuePlot.fileUpload = await ctl.pickFile(ImageSource.camera);
-                                                    ctl.trickImage.refresh();
-                                                  },
-                                                  icon: const Icon(Icons.camera_alt_outlined),
-                                                ),
-                                                const SizedBox(
-                                                  width: 10,
-                                                ),
-                                                IconButton(
-                                                  onPressed: () async {
-                                                    valuePlot.fileUpload = await ctl.pickFile(ImageSource.gallery);
-                                                    ctl.trickImage.refresh();
-                                                  },
-                                                  icon: const Icon(
-                                                    Icons.photo_size_select_actual_rounded,
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                              )
-                            ],
+                          IconButton(
+                            onPressed: () async {
+                              valuePlot.fileUpload = await ctl.pickFile(ImageSource.camera);
+                              ctl.trickImage.refresh();
+                            },
+                            icon: const Icon(Icons.camera_alt_outlined),
+                          ),
+                          IconButton(
+                            onPressed: () async {
+                              valuePlot.fileUpload = await ctl.pickFile(ImageSource.gallery);
+                              ctl.trickImage.refresh();
+                            },
+                            icon: const Icon(
+                              Icons.photo_size_select_actual_rounded,
+                            ),
                           ),
                         ],
                       ),
-                      const Divider(),
-                    ],
-                  );
-                }),
-      ),
+                    ),
+                  ],
+                ),
+        );
+      },
     );
   }
 
